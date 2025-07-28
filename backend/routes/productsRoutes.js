30ede5bafb0318961463f45af046d573a578cdc8
Fusirod <123nguyentranthienphu@gmail.com>
2025-07-28 23:57:30 +0700

    new file:   backend/controllers/dashboardController.js 	modified:   backend/controllers/productController.js 	modified:   backend/controllers/ratingController.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/routes/dashboardRoutes.js 	modified:   backend/routes/productsRoutes.js 	modified:   backend/server.js 	modified:   frontend/src/pages/customer/ProductDetail.css 	modified:   frontend/src/pages/customer/ProductDetail.jsx

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -8,17 +8,16 @@ const {
     createProduct,
     getAllProducts,
     getProductBySlug,
-    createProduct, 
-    updateStockByIds,
-    getStockByIds,
+    //updateStockByIds,
+    //getStockByIds,
     updateProductBySlug,
     togglePublishStatusBySlug,
     deleteProductBySlug
 } = require('../controllers/productController');
 
 router.post('/', createProduct);
-router.patch('/update-stock', updateStockByIds);
-router.get('/get-stock', getStockByIds);
+//router.patch('/update-stock', updateStockByIds);
+//router.get('/get-stock', getStockByIds);
 router.get('/:slug', getProductBySlug); // để GET này cuối
     
 
