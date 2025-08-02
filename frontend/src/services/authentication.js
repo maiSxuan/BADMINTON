a0cde8c0335765bb5ffb6b8cad7576df4d79f311
Mai Xuân <nnmxuan23@clc.fitus.edu.vn>
2025-08-02 23:30:49 +0700

    sửa conflict 2

--- frontend/src/services/authentication.js
+++ frontend/src/services/authentication.js
@@ -31,3 +31,29 @@ export const registerUser = async (data) => {
 
   return await res.json();
 };
+
+export const requestPasswordReset = async (email) => {
+  const res = await fetch("http://localhost:4000/api/auth/forgot-password", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email }),
+  });
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message || "Lỗi gửi email");
+  }
+  return res.json();
+};
+
+export const resetPassword = async (email, token, newPassword) => {
+  const res = await fetch("http://localhost:4000/api/auth/recover-password", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, token, newPassword }),
+  });
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message || "Lỗi đặt lại mật khẩu");
+  }
+  return res.json();
+};
