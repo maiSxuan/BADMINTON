902335186686280a671d51952b13ce9447e44308
Mai Xuân <nnmxuan23@clc.fitus.edu.vn>
2025-08-10 23:28:58 +0700

    thêm slider, chỉnh lại pagination

--- frontend/src/components/customer/Slider.jsx
+++ frontend/src/components/customer/Slider.jsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import "./Slider.css"; 
-import test_img from '../../assets/images/Slider/test.png' //import vào đây cho nhanhz
+import test_img from '../../assets/images/Slider/test1.png' //import vào đây cho nhanhz
 import slide1 from '../../assets/images/Slider/slide1.png'
 import slide2 from '../../assets/images/Slider/slide2.png'
 import slide3 from '../../assets/images/Slider/slide3.png'
