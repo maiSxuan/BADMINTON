2f9f8d15a8bcf09bf029251e205617955c5de3e6
haovo234 <vnhao23@clc.fitus.edu.vn>
2025-08-19 19:29:16 +0700

    tách corsmiddleware

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,11 @@ const express = require('express');
 const jwt = require("jsonwebtoken");
 const path = require("path");
 const bcrypt = require('bcryptjs'); 
+const handleCors = require ('./middleware/corsMiddleware')
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(handleCors);
 
 connectDB();
 const productsRouter = require('./routes/productsRoutes');
